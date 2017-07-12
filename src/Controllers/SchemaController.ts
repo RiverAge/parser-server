6074f494f7fd9946335431029725accf0ef8e010
euky <riverage@163.com>
2017-07-12 13:21:23 +0800

    update schema controller

--- src/Controllers/SchemaController.ts
+++ src/Controllers/SchemaController.ts
@@ -17,6 +17,7 @@
  */
 
 import * as Parse from 'parse/node'
+import SchemaCache from './SchemaCache'
 
 const defaultColumns = Object.freeze({
     // Contains the default columns for every parse object type (except _Join collection)
@@ -115,7 +116,7 @@ const defaultColumns = Object.freeze({
     },
     _GlobalConfig: {
         'objectId': { type: 'String' },
-        'Params': {type: 'Object'}
+        'Params': { type: 'Object' }
     },
     _Audience: {
         'objectId': { type: 'String' },
@@ -129,10 +130,10 @@ const requiredColumns = Object.freeze({
     _Role: ['name', 'ACL']
 })
 
-const systemClasses = Object.freeze(['_User', '_Installation', '_Role', '_Session', '_Product', '_PushStatus', '_JobStatus',
-    '_JobStatus', '_JobSchedule', '_Audience'])
+const systemClasses: ReadonlyArray<string> = ['_User', '_Installation', '_Role', '_Session', '_Product', '_PushStatus', '_JobStatus',
+    '_JobStatus', '_JobSchedule', '_Audience']
 
-const volatileClasses = Object.freeze(['_JobStatus', '_PushStatus', '_Hooks', '_GlobalConfig', '_JobSchedule', '_Audience'])
+const volatileClasses: ReadonlyArray<string> = ['_JobStatus', '_PushStatus', '_Hooks', '_GlobalConfig', '_JobSchedule', '_Audience']
 
 // 10 alpha numeric chars + uppercase
 const userIdRegex = /^[a-zA-Z0-9]{10}/
@@ -158,26 +159,26 @@ const verifyPermissionKey = (key: string) => {
     }
 }
 
-const cLPValidKeys = Object.freeze(['find', 'count', 'get', 'create', 'update', 'delete', 'addField', 'readUserFields', 'writeUserFields'])
-const validateCLP = (perms: { [propName: string]: any }, fields: {[propName: string]: any}) => {
+const cLPValidKeys: ReadonlyArray<string> = ['find', 'count', 'get', 'create', 'update', 'delete', 'addField', 'readUserFields', 'writeUserFields']
+const validateCLP = (perms: { [propName: string]: any }, fields: { [propName: string]: any }) => {
     if (!perms) {
         return
     }
     Object.keys(perms).forEach((operation) => {
-        if (cLPValidKeys.indexOf(operation) === -1 ) {
+        if (cLPValidKeys.indexOf(operation) === -1) {
             throw new Parse.Error(Parse.ErrorCode.INVALID_JSON, `${operation} is not a valid operation for class level permission`)
         }
 
         if (operation === 'readUserFields' || operation === 'writeUserFields') {
             if (!Array.isArray(perms[operation])) {
                 throw new Parse.Error(Parse.ErrorCode.INVALID_JSON,
-                `'${perms[operation]}' is not a valid value for class level permissions ${operation}`)
+                    `'${perms[operation]}' is not a valid value for class level permissions ${operation}`)
             } else {
                 perms[operation].forEach((key: string) => {
-                   if (!fields[key] || fields[key].type !== 'Pointer' || fields[key].targetClass !== '_User') {
-                       throw new Parse.Error(Parse.ErrorCode.INVALID_JSON,
-                       `'${key}' is not valid column for class level pointer permissions ${operation}`)
-                   }
+                    if (!fields[key] || fields[key].type !== 'Pointer' || fields[key].targetClass !== '_User') {
+                        throw new Parse.Error(Parse.ErrorCode.INVALID_JSON,
+                            `'${key}' is not valid column for class level pointer permissions ${operation}`)
+                    }
                 })
             }
             return
@@ -188,7 +189,7 @@ const validateCLP = (perms: { [propName: string]: any }, fields: {[propName: str
             const perm = perms[operation][key]
             if (perm !== true) {
                 throw new Parse.Error(Parse.ErrorCode.INVALID_JSON,
-                `'${perm}' is not a valid for class level permissions ${operation}:${key}:${perm}`)
+                    `'${perm}' is not a valid for class level permissions ${operation}:${key}:${perm}`)
             }
         }))
     })
@@ -227,4 +228,127 @@ const fieldNameIsValidForClass = (fieldName: string, className: String) => {
     return true
 }
 
-const 
+const invalidClassNameMessage = (className: string) => {
+    return 'Invalid class name: ' + className + ', class names can only have alphanumeric characters and _, and must start with an alpha character'
+}
+
+const invalidJsonError = new Parse.Error(Parse.ErrorCode.INVALID_JSON, 'invalid JSON')
+const validNonRelationOrPointerTypes = [
+    'Number',
+    'String',
+    'Boolean',
+    'Date',
+    'Object',
+    'Array',
+    'GeoPoint',
+    'File',
+    'Bytes'
+]
+
+// Returns an error suitable for throwing if the type if invalid
+const fieldTypeIsValid = ({ type, targetClass }: { [props: string]: string }) => {
+    if (['Pointer', 'Relation'].indexOf(type) >= 0) {
+        if (!targetClass) {
+            return new Parse.Error(135, `type ${type} needs a class name`)
+        } else if (typeof targetClass !== 'string') {
+            return invalidJsonError
+        } else if (!classNameIsValid(targetClass)) {
+            return new Parse.Error(Parse.ErrorCode.INVALID_CLASS_NAME, invalidClassNameMessage(targetClass))
+        } else {
+            return undefined
+        }
+    }
+    if (typeof type !== 'string') {
+        return invalidJsonError
+    }
+    if (validNonRelationOrPointerTypes.indexOf(type) < 0) {
+        return new Parse.Error(Parse.ErrorCode.INCORRECT_TYPE, `invalid field type: ${type}`)
+    }
+    return undefined
+}
+
+const convertSchemaToAdapterSchema = (schema) => {
+    schema = injectDefaultSchema(schema)
+    delete schema.field.ACL
+    schema.fields._rperm = { type: 'Array' }
+    schema.fields._wperm = { type: 'Array' }
+
+    if (schema.className === '_User') {
+        delete schema.fields.password
+        schema.fields._hashed_password = { type: 'String' }
+    }
+}
+
+interface InjectSchema {
+    className: string,
+    fields: {},
+    classLevelPermissions?: {}
+}
+const injectDefaultSchema = ({ className, fields, classLevelPermissions }: InjectSchema): InjectSchema => ({
+    className,
+    ...defaultColumns._Default,
+    ...(defaultColumns[className] || {}),
+    classLevelPermissions
+})
+
+/**
+ * Store the entire schema of the app in a weird hybrid format somewhere between
+ * the mongo format and the Parse format. Soon, this will all be Parse format
+ */
+class SchemaController {
+    private _dbAdapter: string
+    private _cache: SchemaCache
+    private reloadDataPromise: Promise<void>
+    private data: string
+    private perms: string
+
+    constructor(dbAdapter: string, schemaCache: SchemaCache) {
+        this._dbAdapter = dbAdapter
+        this._cache = schemaCache
+        // this.data[className][fileName] tells you the type of that field, in mongo format
+        this.data = {}
+        // this.perms[className][operation] tells you the acl-style permissions
+        this.perms = {}
+    }
+
+    reloadData(options = { clearCache: false }) {
+        let promise = Promise.resolve()
+        if (options.clearCache) {
+            promise = promise.then(() => this._cache.clear())
+        }
+
+        if (this.reloadDataPromise && !options.clearCache) {
+            return this.reloadDataPromise
+        }
+
+        this.reloadDataPromise = promise.then(() => this.get)
+    }
+
+    getAllClasses(options = { clearCache: false }) {
+        let promise = Promise.resolve()
+        if (options.clearCache) {
+            promise = this._cache.clear()
+        }
+        return promise.then(() => this._cache.getAllClasses())
+            .then((allClasses) => {
+                if (allClasses && allClasses.length && !options.clearCache) {
+                    return Promise.resolve(allClasses)
+                }
+                return this._dbAdapter.getAllClasses()
+                    .then((allSchemas) => allSchemas.map(injectDefaultSchema))
+                    .then((allSchemas) => this._cache.setAllClasses(allSchemas)
+                        .then(() => allSchemas)
+                    )
+            })
+    }
+
+    getOneSchema(className: string, allowVolatileClasses = false, options = {clearCache: false }) {
+        let promise = Promise.resolve()
+        if (options.clearCache) {
+            promise = this._cache.clear()
+        }
+        return promise.then(() => {
+            if (allowVolatileClasses && volatileClasses.indexOf)
+        })
+    }
+}
