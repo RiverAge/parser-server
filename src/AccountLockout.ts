5917d41a361600b6c7381c3da8c8a9e2f7eb2054
euky <riverage@163.com>
2017-07-14 10:34:15 +0800

    update accountlockout

--- src/AccountLockout.ts
+++ src/AccountLockout.ts
@@ -13,7 +13,7 @@ class AccountLockout {
     /**
      * set _failed_login_count to value
      */
-    private _setFailedLoginCount(value: string) {
+    private _setFailedLoginCount(value: number) {
         const query = {
             username: this._user.username
         }
@@ -31,22 +31,138 @@ class AccountLockout {
     private _isFailedLoginCountSet() {
         const query = {
             username: this._user.username
-            _failed_login_count: {$exists: true}
+            _failed_login_count: { $exists: true }
         }
 
         return this._config.database.find('_User', query)
-        .then((users) => {
-            if (Array.isArray(users) && users.length > 0) {
-                return true
-            }
-            return false
-        })
+            .then((users) => {
+                if (Array.isArray(users) && users.length > 0) {
+                    return true
+                }
+                return false
+            })
     }
 
     /**
      * if _failed_login_count is NOT set then set it to 0 else do nothing
      */
     private _initFailedLoginCount() {
-        return this._
+        return this._isFailedLoginCountSet()
+            .then((failedLoginCountIsSet) => {
+                if (!failedLoginCountIsSet) {
+                    return this._setFailedLoginCount(0)
+                }
+            })
     }
+
+    /**
+     * increment _failed_login_count by 1
+     */
+    private _incrementFailedLoginCount() {
+        const query = {
+            username: this._user.username
+        }
+
+        const updateFields = {
+            _failed_login_count: {
+                __op: 'Increment', amount: 1
+            }
+        }
+
+        return this._config.database.update('_User', query, updateFields)
+    }
+
+    /**
+     * if the failed login count is greater than the threshold
+     * then sets lockout expiration to 'currenttime + accountPolicy.duration',
+     *  i.e.,account is locked out for the next 'accountPolicy.duration' minutes
+     * else do nothing
+     */
+
+    private _setLockoutExpiration() {
+        const query = {
+            username: this._user.username,
+            _failed_login_count: {
+                $gte: this._config.accountLockout.threshold
+            }
+        }
+
+        const now = new Date()
+
+        const updateFields = {
+            _account_lockout_expires_at: Parse._encode(new Date(now.getTime() +
+                this._config.accountLockout.duration * 60 * 1000))
+        }
+
+        return this._config.database.update('_User', query, updateFields)
+            .catch((err) => {
+                if (err && err.code &&
+                    err.message &&
+                    err.code === 101 &&
+                    err.message === 'Object not found.') {
+                    // nothing to update so we are good
+                    return
+                } else {
+                    // unknown err
+                    throw err
+                }
+            })
+    }
+
+    /**
+     * if _account_lockout_expires_at > current_time and _failed_login_count > threshold
+     * reject with account locked error else resolve
+     */
+    private _notLocked() {
+        const query = {
+            username: this._user.username,
+            _account_lockout_expires_at: {
+                $gt: Parse._encode(new Date())
+            },
+            _failed_login_count: {
+                $gte:
+                this._config.accountLockout.threshold
+            }
+        }
+
+        return this._config.database.find('_User', query)
+        .then((users) => {
+            if (Array.isArray(users) && users.length > 0) {
+                throw new Parse.Error(Parse.ErrorCode.OBJECT_NOT_FOUND, 
+                    'Your account is locked due to multiple failed login attempts. Please try again after' +
+                this._config.accountLockout.duration + ' minute(s)')
+            }
+        })
+    }
+
+    /**
+     * set and/or increment _failed_login_count if _failed_login_count > threshold
+     * set the _account_lockout_expires_at to current_time + accountPolicy.duration
+     * else
+     * do nothing
+     */
+
+     private _handleFailedLoginAttempt() {
+         return this._initFailedLoginCount()
+         .then(() => this._incrementFailedLoginCount())
+         .then(() => this._setLockoutExpiration())
+     }
+
+     /**
+      * handle login attempt if the Account Lockout Policy is enabled
+      */
+
+      public handleLoginAttempt(loginSuccessful) {
+        if (!this._config.accountLockout) {
+            return Promise.resolve()
+        }
+        return this._notLocked()
+        .then(() => {
+            if (loginSuccessful) {
+                return this._setFailedLoginCount(0)
+            } else {
+                this._handleFailedLoginAttempt()
+            }
+        })
+      }
 }
